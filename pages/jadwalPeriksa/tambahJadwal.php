a2ad65e3ba2c70d5c5a8aa2620a0bacfe237c17f
A11.2021.13293_Muhammad Alfi Dwi Y <Muhammadalfianto.25@gmail.com>
2025-01-18 00:27:20 +0700

    Dokter Update jadwal

--- pages/jadwalPeriksa/tambahJadwal.php
+++ pages/jadwalPeriksa/tambahJadwal.php
@@ -10,20 +10,17 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $jamMulai = $_POST["jamMulai"];
     $jamSelesai = $_POST["jamSelesai"];
 
-    $queryOverlap = "SELECT * FROM jadwal_periksa INNER JOIN dokter ON jadwal_periksa.id_dokter = dokter.id INNER JOIN poli ON dokter.id_poli = poli.id WHERE id_poli = '$idPoli' AND hari = '$hari' AND ((jam_mulai < '$jamSelesai' AND jam_selesai > '$jamMulai') OR (jam_mulai < '$jamMulai' AND jam_selesai > '$jamMulai'))";
+    // Cek apakah sudah ada jadwal pada hari yang sama untuk dokter yang sama
+    $queryOverlap = "SELECT * FROM jadwal_periksa WHERE id_dokter = '$idDokter' AND hari = '$hari'";
 
-    $resultOverlap = mysqli_query($mysqli,$queryOverlap);
+    $resultOverlap = mysqli_query($mysqli, $queryOverlap);
     
-    if (mysqli_num_rows($resultOverlap)>0) {
-        echo '<script>alert("Dokter lain telah mengambil jadwal ini");window.location.href="../../jadwalPeriksa.php";</script>';
-    }
-    else{
-        // Query untuk menambahkan data obat ke dalam tabel
+    if (mysqli_num_rows($resultOverlap) > 0) {
+        echo '<script>alert("Dokter sudah memiliki jadwal pada hari ini!");window.location.href="../../jadwalPeriksa.php";</script>';
+    } else {
+        // Query untuk menambahkan data jadwal ke dalam tabel
         $query = "INSERT INTO jadwal_periksa (id_dokter, hari, jam_mulai, jam_selesai) VALUES ('$idDokter', '$hari', '$jamMulai', '$jamSelesai')";
         
-
-        // if ($koneksi->query($query) === TRUE) {
-        // Eksekusi query
         if (mysqli_query($mysqli, $query)) {
             echo '<script>';
             echo 'alert("Jadwal berhasil ditambahkan!");';
@@ -31,12 +28,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             echo '</script>';
             exit();
         } else {
-            // Jika terjadi kesalahan, muncul pesan error
             echo "Error: " . $query . "<br>" . mysqli_error($mysqli);
         }
     }
 }
 
-// Tutup koneksi
 mysqli_close($mysqli);
 ?>
